2f571790e3298c59c03fda40ecc05913e2f6128c
jake-bestland <beja0405@gmail.com>
2024-02-22 20:44:26 -0600

    finished lab 02_04

--- 02_more-datatypes/02_04_reorder_numbers.py
+++ 02_more-datatypes/02_04_reorder_numbers.py
@@ -6,3 +6,17 @@
 #
 # Example input:  1,2,3,4,5,6,7,8,9,10
 # Example output: 2,4,6,8,10,9,7,5,3,1
+
+
+count = 10
+num = []
+while count > 0:
+    user_input = int(input("please enter a number: "))
+    num.append(user_input)
+    count -= 1
+    continue
+print(num)
+
+print(num[1], num[3], num[5], num[7], num[9])  
+
+print(num[8], num[6], num[4], num[2], num[0])
