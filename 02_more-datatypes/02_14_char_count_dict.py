4ccfefc2ae09794f6bce4a81be5477ec8bed8178
jake-bestland <beja0405@gmail.com>
2024-02-26 15:30:15 -0600

    finished 02_14

--- 02_more-datatypes/02_14_char_count_dict.py
+++ 02_more-datatypes/02_14_char_count_dict.py
@@ -4,3 +4,8 @@
 #
 # user_input = "hello"
 # result = {"h": 1, "e": 1, "l": 2, "o": 1}
+
+
+user_input = input("please write something here: ")
+result = {x: user_input.count(x) for x in user_input}
+print(result)
