2197349022458a186cf9f2070fefbc829b467a2a
jake-bestland <beja0405@gmail.com>
2024-02-26 12:06:24 -0600

    finished 02_09

--- 02_more-datatypes/02_09_flatten.py
+++ 02_more-datatypes/02_09_flatten.py
@@ -10,3 +10,7 @@
 # to flatten a list of any depth. Can you understand the code used?
 
 starter_list = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+
+
+flattened_list = [num for elem in starter_list for num in elem]
+print(flattened_list)
