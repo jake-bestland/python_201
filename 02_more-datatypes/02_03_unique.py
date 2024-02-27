dc89e8a06b8c058ef14c336123365563905ffe2c
jake-bestland <beja0405@gmail.com>
2024-02-27 12:11:24 -0600

    finished 02_03

--- 02_more-datatypes/02_03_unique.py
+++ 02_more-datatypes/02_03_unique.py
@@ -3,3 +3,13 @@
 #
 # list_ = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13]
 # unique_list = [55, 'hi', 4, 13]
+
+
+a = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13]
+
+b = []
+for x in a:
+    if a.count(x) <= 1:
+        b.append(x)
+print(b)
+
