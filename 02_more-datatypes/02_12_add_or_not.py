36963d5c869219909e55ab89c80843370d64e4eb
jake-bestland <beja0405@gmail.com>
2024-02-27 11:23:12 -0600

    finished 02_12

--- 02_more-datatypes/02_12_add_or_not.py
+++ 02_more-datatypes/02_12_add_or_not.py
@@ -6,3 +6,32 @@
 # their input is a duplicate and deduct a point.
 # If the user loses 5 points, quit the program.
 # They win if they manage to create a set that has more than 10 items.
+
+points = 11
+lives = 5
+numbers = set()
+print("Welcome to my memory game!  To win the game, enter enough new numbers without any repeats.  Enter too many duplicate numbers and you will lose!")
+while True:
+    if lives == 0:
+        print("I'm sorry, you are out of lives.  You lose!")
+        break
+    if points == 0:
+        print("Congratulations, You win!")
+        break
+    
+    user_input = (input("Please enter a number: "))
+    
+    if user_input.isdigit() == False:
+        print("I'm sorry, that isn't a number.  Try again.")
+        continue
+    elif user_input in numbers:
+        print(f"You have already entered that number!  Try again with a new number, you have {lives} lives remaining!")
+        lives -= 1
+        continue
+    else:
+        numbers.update(user_input)
+        print(f"Good job! you have not entered that number yet!  You need {points} more points until you win!")
+        points -= 1
+        continue
+
+
