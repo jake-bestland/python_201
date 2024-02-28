6fecd8044bcfa41ae36130135b32a8bc5501f42b
jake-bestland <beja0405@gmail.com>
2024-02-28 15:26:49 -0600

    added file counter

--- projects/file_counter.py
+++ projects/file_counter.py
@@ -1,1 +1,12 @@
 # Add the code for the file counter script that you wrote in the course.
+import pathlib
+
+desktop = pathlib.Path("/Users/jakebestland/Desktop")
+
+desk_files = []
+
+for filepath in desktop.iterdir():
+    desk_files.append(filepath.suffix)
+
+desk_dict = {x: desk_files.count(x) for x in desk_files}
+print(desk_dict)
