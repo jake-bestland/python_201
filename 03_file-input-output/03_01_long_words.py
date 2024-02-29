33b0331d6ee2e4a60724a7cc2d588cd11239eb2d
jake-bestland <beja0405@gmail.com>
2024-02-29 14:38:26 -0600

    finished 03_01

--- 03_file-input-output/03_01_long_words.py
+++ 03_file-input-output/03_01_long_words.py
@@ -1,2 +1,17 @@
 # Write a program that reads in `words.txt` and prints only the words
 # that have more than 20 characters (not counting whitespace).
+
+from pathlib import Path
+
+word_path = Path("/Users/jakebestland/Documents/codingnomads/python-201-main/03_file-input-output/words.txt")
+
+with word_path.open() as word_file:
+    #print(word_file.read())
+
+    long_words = []
+    for w in word_file:
+        w = w.rstrip()
+        if len(w) > 20:
+            long_words.append(w)
+
+    print("\n".join(long_words))
