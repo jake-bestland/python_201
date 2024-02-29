28e0e4cbd003f40c0732e43334346b50fb96d326
jake-bestland <beja0405@gmail.com>
2024-02-29 16:46:20 -0600

    finished 03_02

--- 03_file-input-output/03_02_words_analysis.py
+++ 03_file-input-output/03_02_words_analysis.py
@@ -4,3 +4,35 @@
 # 1. The shortest word (if there is a tie, print all)
 # 2. The longest word (if there is a tie, print all)
 # 3. The total number of words in the file.
+
+
+from pathlib import Path
+
+word_path = Path("/Users/jakebestland/Documents/codingnomads/python-201-main/03_file-input-output/words.txt")
+
+short = 5
+short_word = []
+long = 0
+long_word = []
+w_count = []
+with word_path.open() as word_file:
+    for w in word_file:
+        w = w.strip()
+        if len(w) < short:
+            short = len(w)
+            short_word.clear()
+            short_word.append(w)
+        elif len(w) == short:
+            short_word.append(w)
+        
+        if len(w) > long:
+            long = len(w)
+            long_word.clear()
+            long_word.append(w)
+        elif len(w) == long:
+            long_word.append(w)
+        
+        w_count.append(w)
+print(short_word)
+print(long_word)
+print(len(w_count))
