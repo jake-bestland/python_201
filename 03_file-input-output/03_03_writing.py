3bd517e36cf6b9d5e6fbcc1aa40ae67362b2cb2f
jake-bestland <beja0405@gmail.com>
2024-02-29 17:12:50 -0600

    finished 03_03

--- 03_file-input-output/03_03_writing.py
+++ 03_file-input-output/03_03_writing.py
@@ -1,2 +1,20 @@
 # Write a script that reads in the contents of `words.txt`
 # and writes the contents in reverse to a new file `words_reverse.txt`.
+
+
+from pathlib import Path
+
+word_path = Path("/Users/jakebestland/Documents/codingnomads/python-201-main/03_file-input-output/words.txt")
+
+
+w_list = []
+with word_path.open() as word_file:
+    for w in word_file:
+        w = w.strip()
+        w_list.append(w)
+
+w_list.reverse()
+print("\n".join(w_list))
+
+with open("words_reverse.txt", "w") as r_list:
+     r_list.write("\n".join(w_list))
