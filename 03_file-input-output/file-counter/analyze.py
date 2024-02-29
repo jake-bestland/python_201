93ae59075e2a704eb8c61191f4d07dba8ce0c640
jake-bestland <beja0405@gmail.com>
2024-02-29 14:57:27 -0600

    add read/write csv file for filecounter

--- 03_file-input-output/file-counter/analyze.py
+++ 03_file-input-output/file-counter/analyze.py
@@ -1,1 +1,23 @@
 # Use the `csv` module to read in and count the different file types.
+import pathlib
+import csv
+
+desktop = pathlib.Path("/Users/jakebestland/Desktop")
+
+desk_files = []
+
+for filepath in desktop.iterdir():
+    desk_files.append(filepath.suffix)
+
+count = {x: desk_files.count(x) for x in desk_files}
+print(count)
+
+with open("filecounts.csv", "r") as csvfile:
+    reader = csv.DictReader(csvfile, fieldnames=["Folder", "PY", "PNG", "TXT", "CSV"])
+    counts2 = list(reader)
+print(counts2[-1])
+
+
+
+
+
--- 03_file-input-output/file-counter/csvwrite.py
+++ 03_file-input-output/file-counter/csvwrite.py
@@ -1,1 +1,19 @@
 # Write the file counts to a `.csv` file.
+
+import pathlib
+import csv
+
+desktop = pathlib.Path("/Users/jakebestland/Desktop")
+
+desk_files = []
+
+for filepath in desktop.iterdir():
+    desk_files.append(filepath.suffix)
+
+count = {x: desk_files.count(x) for x in desk_files}
+print(count)
+
+with open("filecounts.csv", "a") as csvfile:
+    countwriter = csv.writer(csvfile)
+    data = [count[""], count[".py"], count[".png"], count[".txt"], count[".csv"]]
+    countwriter.writerow(data)
