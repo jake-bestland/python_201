391cc6edb05c5fe6de8c0ec497755a4c92d57dc0
jake-bestland <beja0405@gmail.com>
2024-03-04 14:14:59 -0600

    finished 04_03

--- 04_functions-and-scopes/04_03_write_letter.py
+++ 04_functions-and-scopes/04_03_write_letter.py
@@ -3,3 +3,13 @@
 # message with the `name` input, as well as a goodbye message that uses
 # the `name` again. Combine that with the input `text` to return a
 # complete `letter`.
+
+
+def write_letter(name, text):
+    greeting = f"Hello {name}! How are you?"
+    goodbye =  f"Goodbye {name}! I hope to talk to you again soon!"
+    message = f"{text}"
+    letter = f"{greeting} {message} {goodbye}"
+    return letter
+
+print(write_letter("Jake", "We should go to a hockey game next weekend!"))
