1817e7f6694479eceb79c8d4680a6026cbf65cf8
jake-bestland <beja0405@gmail.com>
2024-03-05 10:55:34 -0600

    finished 04_10

--- 04_functions-and-scopes/04_10_type_annotations.py
+++ 04_functions-and-scopes/04_10_type_annotations.py
@@ -1,12 +1,25 @@
 # Add type annotations to the three functions shown below.
 
-def multiply(num1, num2):
+# def multiply(num1, num2):
+#     return num1 * num2
+
+# def greet(greeting, name):
+#     sentence = f"{greeting}, {name}! How are you?"
+#     return sentence
+
+# def shopping_list(*args):
+#     [print(f"* {item}") for item in args]
+#     return args
+
+###############################
+
+def multiply(num1: int, num2: int) -> int:
     return num1 * num2
 
-def greet(greeting, name):
+def greet(greeting: str, name: str) -> str:
     sentence = f"{greeting}, {name}! How are you?"
     return sentence
 
-def shopping_list(*args):
+def shopping_list(*args: list[str]) -> list[str]:
     [print(f"* {item}") for item in args]
     return args
