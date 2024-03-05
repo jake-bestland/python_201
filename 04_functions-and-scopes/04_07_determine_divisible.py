cc65305f42dbcc25404b99881b1bb613a8e071b8
jake-bestland <beja0405@gmail.com>
2024-03-05 11:17:54 -0600

    cleaned up return value in both docstrings.  changed var name of user input

--- 04_functions-and-scopes/04_07_determine_divisible.py
+++ 04_functions-and-scopes/04_07_determine_divisible.py
@@ -8,7 +8,6 @@
 #   and set their output equal to new variables 
 # - print your the result variables with descriptive messages
 
-
 def div_4_or_7(number):
     """Check if a number is divisible by either 4 or 7.
 
@@ -16,7 +15,7 @@ def div_4_or_7(number):
         number (int): the number you want to check if it is divisible by 4 or 7
 
     Returns:
-        bool: a true if it is or false if it is not
+        bool: a true or false value
     """
     correct = True
     if number % 4 == 0 or number % 7 ==0:
@@ -32,7 +31,7 @@ def div_both_4_7(number):
         number (int): the number you want to check if it is divisible by both 4 and 7
 
     Returns:
-        bool: true if it is or false if it is not
+        bool: a true or false value
     """
     correct = True
     if number % 4 == 0 and number % 7 == 0:
@@ -41,14 +40,12 @@ def div_both_4_7(number):
         answer = not correct
     return answer
 
-
-
 while True:
-    number = input("Select a number between 1 and 1,000,000,000: ")
-    if not number.isdigit():
+    user_num = input("Select a number between 1 and 1,000,000,000: ")
+    if not user_num.isdigit():
         print("Sorry, that is not a number. Please enter a number.")
         continue
-    number = int(number)
+    number = int(user_num)
     if number < 1 or number > 1000000000:
         print("Sorry, that number is out side the range. Please pick a number in the range.")
         continue
