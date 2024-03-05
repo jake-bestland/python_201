b6069f185087a4cab5c386c0f902f766e8c68f3b
jake-bestland <beja0405@gmail.com>
2024-03-05 10:30:50 -0600

    finished 04_08

--- 04_functions-and-scopes/04_08_calculate_stats.py
+++ 04_functions-and-scopes/04_08_calculate_stats.py
@@ -1,11 +1,26 @@
 # Write a function named `stats()` that takes in a list of numbers
 # and finds the maximum, minimum, average and sum of the numbers.
 # Print these values to the console you call the function.
+from statistics import mean
 
 example_list = [1, 2, 3, 4, 5, 6, 7]
 
-def stats():
+def stats(list_of_num):
   # define the function here
-  pass
+  """Takes a list of numbers and finds the maximum, minimum, average and sum of the numbers. 
+
+  Args:
+      list_of_num (list): A list of numbers
+
+  Returns:
+      str: a string displaying the maximum, minimum, average and sum of the numbers.
+  """
+  max_num = max(list_of_num)
+  min_num = min(list_of_num)
+  avg = mean(list_of_num)
+  list_sum = sum(list_of_num)
+  values = f"maximum = {max_num}, minimum = {min_num}, average = {avg}, sum = {list_sum}"
+  return values
 
 # call the function below here
+print(stats(example_list))
