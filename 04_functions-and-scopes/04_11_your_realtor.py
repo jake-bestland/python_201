aac28ac92dbf9ad2e037a0e2841ad97e7371bc85
jake-bestland <beja0405@gmail.com>
2024-03-05 13:24:38 -0600

    finished 04_11

--- 04_functions-and-scopes/04_11_your_realtor.py
+++ 04_functions-and-scopes/04_11_your_realtor.py
@@ -3,3 +3,27 @@
 # is why your function should be able to take an arbitrary amount of
 # keyword arguments, and display them all in a list form with some 
 # introductory information.
+
+
+def features(**kwargs: dict[str, int]) -> str:
+    """ Lists Features and their amount.
+
+    Args:
+        **kwargs (dict[str, int]): a dictionary with a 'str' key and 'int' value.
+        dict[str]: features of the listing that have a value. e.g. "Bedrooms", "Bathrooms", "Acres", or "Sq. ft."
+        dict[int]: the amount of the features.
+
+    Returns:
+        str: an introductory sentence with a list of the features and their values formated nicely.
+    """
+    print(f"Here are some of the main features for this listing: \n")
+    pros = ""
+    for feat, amt in kwargs.items():
+        sentence = f"* {feat}: {amt} \n"
+        pros += sentence + "\n"
+
+    return pros
+
+feats = {"Bedrooms": 3, "Bathrooms": 2, "Acres": 2, "Sq. Ft.": 3500}
+
+print(features(**feats))
