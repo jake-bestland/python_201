cc6ee39595d6ca492f772532d8626d3440736b78
jake-bestland <beja0405@gmail.com>
2024-03-05 09:40:32 -0600

    finished 04_07

--- 04_functions-and-scopes/04_07_determine_divisible.py
+++ 04_functions-and-scopes/04_07_determine_divisible.py
@@ -7,3 +7,61 @@
 # - call your functions, passing in the user input as the arguments,
 #   and set their output equal to new variables 
 # - print your the result variables with descriptive messages
+
+
+def div_4_or_7(number):
+    """Check if a number is divisible by either 4 or 7.
+
+    Args:
+        number (int): the number you want to check if it is divisible by 4 or 7
+
+    Returns:
+        bool: a true if it is or false if it is not
+    """
+    correct = True
+    if number % 4 == 0 or number % 7 ==0:
+        answer = correct
+    else:
+        answer = not correct
+    return answer
+
+def div_both_4_7(number):
+    """check if a number is divisible by both 4 and 7.
+
+    Args:
+        number (int): the number you want to check if it is divisible by both 4 and 7
+
+    Returns:
+        bool: true if it is or false if it is not
+    """
+    correct = True
+    if number % 4 == 0 and number % 7 == 0:
+        answer = correct
+    else:
+        answer = not correct
+    return answer
+
+
+
+while True:
+    number = input("Select a number between 1 and 1,000,000,000: ")
+    if not number.isdigit():
+        print("Sorry, that is not a number. Please enter a number.")
+        continue
+    number = int(number)
+    if number < 1 or number > 1000000000:
+        print("Sorry, that number is out side the range. Please pick a number in the range.")
+        continue
+    if number >= 1 and number <= 1000000000:
+        if div_4_or_7(number) == True:
+            answer1 = "is"
+        else:
+            answer1 = "is not"
+        if div_both_4_7(number) == True:
+            answer_both = "is"
+        else:
+            answer_both = "is not"
+        break
+
+print(f"The number you have selected {answer1} divisible by EITHER 4 OR 7!")
+print(f"The number you have selected {answer_both} divisible by BOTH 4 AND 7!")
