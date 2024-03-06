acde8c83e8965d172c733b7d194a89638c3b24a3
jake-bestland <beja0405@gmail.com>
2024-03-06 17:48:37 -0600

    finished mini project

--- projects/my_enumerate.py
+++ projects/my_enumerate.py
@@ -2,6 +2,16 @@
 # Define a function called `my_enumerate()` that takes an iterable as input
 # and gives back both the element as well as its index position as an integer.
 
-def my_enumerate():  # add your arguments
+def my_enumerate(iterable, start=0):  # add your arguments
       # add your code implementation
-      pass
+      n = start
+      for elem in iterable:
+            yield n, elem
+            n += 1
+
+
+months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
+
+# print(list(my_enumerate(months, 1)))
+for index, months in my_enumerate(months, 1):
+    print(f"{index}: {months}")
