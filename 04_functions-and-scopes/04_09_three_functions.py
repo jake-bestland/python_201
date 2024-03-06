811dc31c07b09433433508ddcfb1e56400ac7f43
jake-bestland <beja0405@gmail.com>
2024-03-06 13:58:47 -0600

    finished 04_09

--- 04_functions-and-scopes/04_09_three_functions.py
+++ 04_functions-and-scopes/04_09_three_functions.py
@@ -3,3 +3,42 @@
 # of that function to do something with it. You can have more
 # than three functions, and they don't need to call each other
 # in a circular way.
+
+# seasons_list = list(input("What are the four seasons? ").split())
+# for index, season in enumerate(seasons_list):
+#     print(f"{index}: {season}")
+
+
+
+def greet(greeting: str, name: str) -> str:
+    """Generates a greeting."""
+    sentence = f"{greeting}, {name.capitalize()}!"
+    return sentence
+
+def sarcasm(text):
+    result = ""
+    for char in range(len(text)):
+        if not char % 2:
+            result = result + text[char].upper()
+        else:
+            result = result + text[char].lower()
+    return result
+
+def angry(text):
+    sentence = ""
+    for word in text.split():
+        sentence += word.upper()
+    return sentence
+
+
+
+name = input("What is your first name? ")
+
+intro = greet("Hello", name)
+print(intro)
+silly = sarcasm(intro)
+print(silly)
+yell = angry(silly)
+print(yell)
+
+
