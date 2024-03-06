468cbcf924b0e5d9e01e9e75708438b0867a4b26
jake-bestland <beja0405@gmail.com>
2024-03-06 14:10:27 -0600

    import path. changed csv filepath. added reader

--- projects/file_counter.py
+++ projects/file_counter.py
@@ -1,8 +1,8 @@
 # Add the code for the file counter script that you wrote in the course.
-import pathlib
+from pathlib import Path
 import csv
 
-desktop = pathlib.Path("/Users/jakebestland/Desktop")
+desktop = Path("/Users/jakebestland/Desktop")
 
 desk_files = []
 
@@ -12,29 +12,20 @@ for filepath in desktop.iterdir():
 count = {x: desk_files.count(x) for x in desk_files}
 print(count)
 
-# data = f"{count[""]}, {count[".py"]}, {count[".png"]}, {count[".txt"]}, {count[".csv"]}"
-# file_out = open("filecounts.csv", "a")
-# file_out.write(f"{data}")
-# file_out.write("\n")
-# file_out.close()
 
-# file_in = open("output.txt", "r")
-# contents = file_in.read()
-# print(contents)
-# file_in.close()
+csvfile_path = Path("/Users/jakebestland/Desktop/filecounts.csv")
 
-with open("filecounts.csv", "a") as csvfile:
+with open(csvfile_path.joinpath(), "a") as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=count)
     writer.writeheader()
     writer.writerow(count)
-    
-    # countwriter = csv.writer(csvfile)
-    # data = [count[""], count[".py"], count[".png"], count[".txt"], count[".csv"]]
-    # countwriter.writerow(data)
 
-# # file_out = open("filecounts.csv", "a")
-# # file_out.write(str(count))
-# with open("filecounts.csv", "a") as csvfile:
-#     countwriter = csvfile.writer(count)
-#     data = [count[""], count[".py"], count[".png"], count[".txt"], count[".csv"]]
+# with open(csvfile_path.joinpath(), "a") as csvfile:
+#     countwriter = csv.writer(csvfile)
+#     data = [count[".py"], count[""], count[".png"], count[".txt"], count[".csv"]]
 #     countwriter.writerow(data)
+
+with csvfile_path.open() as csvfile:
+    reader = csv.DictReader(csvfile, fieldnames=["PY", "Folder", "PNG", "TXT", "CSV"])
+    counts2 = list(reader)
+print(counts2)
