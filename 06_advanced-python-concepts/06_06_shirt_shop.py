d105777868098bbe62c3d03f1a63dd23623478ef
jake-bestland <beja0405@gmail.com>
2024-03-13 14:20:44 -0500

    finished 06_06

--- 06_advanced-python-concepts/06_06_shirt_shop.py
+++ 06_advanced-python-concepts/06_06_shirt_shop.py
@@ -3,3 +3,13 @@
 
 colors = ["neon orange", "spring green"]
 sizes = ["S", "M", "L"]
+
+cart = [(x, y) for x in colors for y in sizes]
+print(cart)
+
+
+# different formatting#
+
+# c1 = [(colors[0], y) for y in sizes]
+# c2 = [(colors[1], y) for y in sizes]
+# print(f"{c1}\n{c2}")
