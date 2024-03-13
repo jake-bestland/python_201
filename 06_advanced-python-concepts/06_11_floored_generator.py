9278ca1c75e09931e3a8103b5df3ae351b7e1e3b
jake-bestland <beja0405@gmail.com>
2024-03-13 17:25:28 -0500

    finished 06_11

--- 06_advanced-python-concepts/06_11_floored_generator.py
+++ 06_advanced-python-concepts/06_11_floored_generator.py
@@ -1,3 +1,14 @@
 # Adapt your Generator expression from the previous exercise:
 # Add a floor division by 1111 on it.
 # What numbers do you get?
+
+
+
+nums = range(1, 1000000)
+
+
+div_1111 = (x for x in nums if x % 1111 == 0)
+for x in div_1111:
+    print(x // 1111)
+
+# 1-900 #    
