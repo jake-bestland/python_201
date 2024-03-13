e283d7ef80a394788379d506f2d8b342a4031edf
jake-bestland <beja0405@gmail.com>
2024-03-13 18:20:18 -0500

    added 2nd way to complete

--- 06_advanced-python-concepts/06_12_lambda_sum.py
+++ 06_advanced-python-concepts/06_12_lambda_sum.py
@@ -1,6 +1,10 @@
 # Write a lambda expression that takes in three numbers
 # and returns the sum of the numbers.
 
+sum1 = (lambda a, b, c: a + b + c)
+print(sum1(1, 2, 3))
 
-sum1 = (lambda a, b, c: a + b + c)(1, 2, 3)
-print(sum1)
+#or
+
+# sum1 = (lambda a, b, c: a + b + c)(1, 2, 3)
+# print(sum1)
