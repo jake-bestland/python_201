6e9dc69a69eae8d64de4ebf8c7afc3bc6bdbf5ec
jake-bestland <beja0405@gmail.com>
2024-03-14 12:51:03 -0500

    finished 06_08

--- 06_advanced-python-concepts/06_08_based_dictcomp.py
+++ 06_advanced-python-concepts/06_08_based_dictcomp.py
@@ -12,3 +12,15 @@
 # {0: [0, 0, 0], 1: [0, 0, 1], 2: [0, 1, 0], 3: [0, 1, 1], ...,
 # 7: [1, 1, 1], 8: [1, 0, 0, 0], 9: [1, 0, 0, 1], ...,
 # 999: [1, 1, 1, 1, 1, 0, 0, 1, 1, 1]}
+
+
+num_dict = {i: [int(x) for x in list(str(i))] for i in range(999)}
+print(num_dict)
+
+
+
+# for i in range(999):
+#     digits = list(str(i))
+#     #print(digits)
+#     i_dig = [int(x) for x in list(str(i))]
+#     print(i_dig)
