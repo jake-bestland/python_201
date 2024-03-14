0ac44be1f3412c29c442343a922d7bdbf0e042c1
jake-bestland <beja0405@gmail.com>
2024-03-14 12:53:08 -0500

    finished 06_13 and 06_14

--- 06_advanced-python-concepts/06_13_void_return.py
+++ 06_advanced-python-concepts/06_13_void_return.py
@@ -1,2 +1,10 @@
 # Write a lambda function that does not take in an arguments 
 # but returns a value. Print the return value.
+
+# void = lambda x: x*2
+# print(void)
+
+## prints: <function <lambda> at 0x100c1d080>
+
+greet = lambda : 'Hello World!'
+print(greet())
--- 06_advanced-python-concepts/06_14_hello_lambda.py
+++ 06_advanced-python-concepts/06_14_hello_lambda.py
@@ -1,2 +1,10 @@
 # Create a lambda expression that takes no input
 # and prints "hello world" to the console. What does it return?
+
+
+
+# text = (lambda h, w: h + w)('hello ', 'world ')
+# print(text)
+
+greet = lambda : print(f'"hello world"')
+print(greet())
