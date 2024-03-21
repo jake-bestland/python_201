22fc9cbce691ad2386d6abdf8a4f48a1674f850b
jake-bestland <beja0405@gmail.com>
2024-03-21 15:47:17 -0500

    created filecount script to write into database

---
+++ projects/File_count_database.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import sqlalchemy
+
+desktop = Path("/Users/jakebestland/Desktop")
+
+engine = sqlalchemy.create_engine('mysql+pymysql://root:GuttersRule!5@localhost/desk_file_count')
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
+
+new_table = sqlalchemy.Table('Filecounter', metadata, autoload_with=engine)
+
+query = sqlalchemy.insert(new_table)  
+for filepath in desktop.iterdir():
+    new_records = [{'file_name': filepath.name, 'file_type': filepath.suffix}] 
+    result_proxy = connection.execute(query,new_records)
+
+connection.commit()
+
+
