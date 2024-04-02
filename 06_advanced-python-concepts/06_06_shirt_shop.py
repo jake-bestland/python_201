dffe49416e3f53761bf9306f0158b35dbf937f32
jake-bestland <beja0405@gmail.com>
2024-04-02 14:47:58 -0500

    deleted other format

--- 06_advanced-python-concepts/06_06_shirt_shop.py
+++ 06_advanced-python-concepts/06_06_shirt_shop.py
@@ -6,10 +6,3 @@ sizes = ["S", "M", "L"]
 
 cart = [(x, y) for x in colors for y in sizes]
 print(cart)
-
-
-# different formatting#
-
-# c1 = [(colors[0], y) for y in sizes]
-# c2 = [(colors[1], y) for y in sizes]
-# print(f"{c1}\n{c2}")
