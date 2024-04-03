7eb55a6f19a2dac4d690cb012d80740a7b086929
jake-bestland <beja0405@gmail.com>
2024-04-03 10:35:13 -0500

    finished project

--- projects/barking-shibes.py
+++ projects/barking-shibes.py
@@ -7,3 +7,20 @@
 # Write the code logic to make the API calls and assign the output to
 # `doged_quote` and `doge_image_url` respectively.
 # Then write the `html` string to a `.html` file and look at it in your browser.
+from pathlib import Path
+import requests
+
+quote_url = "https://api.quotable.io/quotes/random"
+quote = requests.get(quote_url).json()[0]["content"]
+
+doged_quote = f"Wow. {quote.upper()}. Much wisdom."
+
+
+doge_img_url = "http://shibe.online/api/shibes"
+doge_img = requests.get(doge_img_url).json()[0]
+
+
+html = f"<p style='font-family:Comic Sans'>{doged_quote}</p><img src='{doge_img}'>"
+
+file = Path().home().joinpath("Desktop").joinpath("doge.html")
+file.write_text(html)
